c52b417487734962cb04099651a7df766bdcb101
MaazMS <maazshaikh7020@gmail.com>
2021-09-13 12:33:38 +0530

    modify namespace yaml

--- hack/generate_workload.sh
+++ hack/generate_workload.sh
@@ -13,7 +13,7 @@ curl -L -o yq_linux.tar.gz https://github.com/mikefarah/yq/releases/download/v4.
  sudo mv yq_linux_amd64 /usr/local/bin/yq && \
  rm yq_linux.tar.gz
 
-kubectl create namespace $NAMESPACE && kubectl label namespace $NAMESPACE $LABELNAME=$LABELVALUE  --dry-run -o yaml  | yq eval 'del(.metadata.creationTimestamp, .metadata.managedFields, .metadata.resourceVersion, .metadata.selfLink, .metadata.selfLink, .metadata.uid, .spec, .status)' - >> $OUTPUT_YAML
+kubectl create namespace $NAMESPACE  && kubectl label namespace $NAMESPACE  $LABELNAME=$LABELVALUE  --dry-run -o yaml | yq eval 'del(.metadata.creationTimestamp, .metadata.managedFields, .metadata.resourceVersion, .metadata.selfLink, .metadata.selfLink, .metadata.uid, .spec, .status)' - >> $OUTPUT_YAML
 
 CRD_LOC="./charts/workshop-operator/crds"
 echo $CRD_LOC
