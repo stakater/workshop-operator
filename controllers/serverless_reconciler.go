0c5a7360fb9a8045118ac44d5cd8158fae200821
MaazMS <maazshaikh7020@gmail.com>
2021-09-15 16:14:49 +0530

    delete method of serverless

--- controllers/serverless_reconciler.go
+++ controllers/serverless_reconciler.go
@@ -2,11 +2,13 @@ package controllers
 
 import (
 	"context"
-
+	olmv1alpha1 "github.com/operator-framework/api/pkg/operators/v1alpha1"
 	"github.com/prometheus/common/log"
 	workshopv1 "github.com/stakater/workshop-operator/api/v1"
 	"github.com/stakater/workshop-operator/common/kubernetes"
 	"github.com/stakater/workshop-operator/common/util"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -22,7 +24,11 @@ func (r *WorkshopReconciler) reconcileServerless(workshop *workshopv1.Workshop) 
 			return result, err
 		}
 	}
-
+	if enabledServerless {
+		if result, err := r.deleteServerless(workshop); util.IsRequeued(result, err) {
+			return result, err
+		}
+	}
 	//Success
 	return reconcile.Result{}, nil
 }
@@ -65,6 +71,70 @@ func (r *WorkshopReconciler) addServerless(workshop *workshopv1.Workshop) (recon
 	// TODO
 	// Add  knativeServingNamespace to ServiceMeshMember
 	// Create CR
+
+	//Success
+	return reconcile.Result{}, nil
+}
+
+// delete Serverless
+func (r *WorkshopReconciler) deleteServerless(workshop *workshopv1.Workshop) (reconcile.Result, error) {
+
+	channel := workshop.Spec.Infrastructure.Serverless.OperatorHub.Channel
+	clusterServiceVersion := workshop.Spec.Infrastructure.Serverless.OperatorHub.ClusterServiceVersion
+
+	namespace := kubernetes.NewNamespace(workshop, r.Scheme, "openshift-serverless")
+	knativeServingNamespace := kubernetes.NewNamespace(workshop, r.Scheme, "knative-serving")
+
+	knativeEventingNamespace := kubernetes.NewNamespace(workshop, r.Scheme, "knative-eventing")
+	knativeEventingNamespaceFound := &corev1.Namespace{}
+	knativeEventingNamespaceErr := r.Get(context.TODO(), types.NamespacedName{Name: knativeEventingNamespace.Name}, knativeEventingNamespaceFound)
+	if knativeEventingNamespaceErr == nil {
+		//Delete knativeEventing Namespace
+		if err := r.Delete(context.TODO(), knativeEventingNamespace); err != nil {
+			return reconcile.Result{}, err
+		}
+		log.Infof("Deleted %s Namespace", knativeEventingNamespace.Name)
+	}
+
+	knativeServingNamespaceFound := &corev1.Namespace{}
+	knativeServingNamespaceErr := r.Get(context.TODO(), types.NamespacedName{Name: knativeServingNamespace.Name}, knativeServingNamespaceFound)
+	if knativeServingNamespaceErr == nil {
+		//Delete knativeServing Namespace
+		if err := r.Delete(context.TODO(), knativeServingNamespace); err != nil {
+			return reconcile.Result{}, err
+		}
+		log.Infof("Deleted %s Namespace", knativeServingNamespace.Name)
+	}
+
+	subscription := kubernetes.NewRedHatSubscription(workshop, r.Scheme, "serverless-operator", namespace.Name, "serverless-operator",
+		channel, clusterServiceVersion)
+	subscriptionFound := &olmv1alpha1.Subscription {}
+	subscriptionErr := r.Get(context.TODO(), types.NamespacedName{Name: subscription.Name, Namespace: namespace.Name}, subscriptionFound)
+	if subscriptionErr == nil {
+		//Delete subscription
+		if err := r.Delete(context.TODO(),subscription); err != nil {
+			return reconcile.Result{}, err
+		}
+		log.Infof("Deleted %s Subscription", subscription.Name)
+	}
+
+	namespaceFound := &corev1.Namespace{}
+	namespaceErr := r.Get(context.TODO(), types.NamespacedName{Name: namespace.Name}, namespaceFound)
+	if namespaceErr == nil {
+		// Delete namespace
+		if err := r.Delete(context.TODO(),namespace ); err != nil{
+			return reconcile.Result{}, err
+		}
+		log.Infof("Deleted %s namespace", namespace.Name)
+	}
+
+	//
+
+
+
+	// TODO
+	// Add  knativeServingNamespace to ServiceMeshMember
+	// Delete CR
 
 	//Success
 	return reconcile.Result{}, nil
