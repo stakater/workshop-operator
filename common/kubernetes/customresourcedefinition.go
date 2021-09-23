bb3d8810bc4e91abc5bcea4a438a79668aad445d
MaazMS <maazshaikh7020@gmail.com>
2021-09-23 12:27:51 +0530

    add method GetCustomResourceDefinition

--- common/kubernetes/customresourcedefinition.go
+++ common/kubernetes/customresourcedefinition.go
@@ -1,10 +1,12 @@
 package kubernetes
 
 import (
+	"github.com/prometheus/common/log"
 	workshopv1 "github.com/stakater/workshop-operator/api/v1"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 // NewCustomResourceDefinition creates a Custom Resource Definition (CRD)
@@ -34,13 +36,37 @@ func NewCustomResourceDefinition(workshop *workshopv1.Workshop, scheme *runtime.
 	}
 
 	// Set Workshop instance as the owner and controller
-	/**
-	Error: cluster-scoped resource must not have a namespace-scoped owner
 	err := ctrl.SetControllerReference(workshop, crd, scheme)
 	if err != nil {
 		log.Error(err, " - Failed to set SetControllerReference for CRD - %s", name)
 	}
-	*/
+	return crd
+}
 
+
+// GetCustomResourceDefinition return Custom Resource Definition (CRD)
+func GetCustomResourceDefinition(name string, group string, kind string, listKind string, plural string, singular string, version string, shortNames []string, additionalPrinterColumns []apiextensionsv1beta1.CustomResourceColumnDefinition) *apiextensionsv1beta1.CustomResourceDefinition {
+
+	crd := &apiextensionsv1beta1.CustomResourceDefinition{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
+			Group:   group,
+			Version: version,
+			Scope:   apiextensionsv1beta1.NamespaceScoped,
+			Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
+				Kind:       kind,
+				ListKind:   listKind,
+				Plural:     plural,
+				Singular:   singular,
+				ShortNames: shortNames,
+			},
+			Subresources: &apiextensionsv1beta1.CustomResourceSubresources{
+				Status: &apiextensionsv1beta1.CustomResourceSubresourceStatus{},
+			},
+			AdditionalPrinterColumns: additionalPrinterColumns,
+		},
+	}
 	return crd
 }
