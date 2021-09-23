4e3164c7c6aaa4365a4ee96c07e7765f14f46e39
MaazMS <maazshaikh7020@gmail.com>
2021-09-23 12:27:08 +0530

    add method GetClusterRoleBindingSA

--- common/kubernetes/clusterrolebinding.go
+++ common/kubernetes/clusterrolebinding.go
@@ -1,10 +1,12 @@
 package kubernetes
 
 import (
+	"github.com/prometheus/common/log"
 	workshopv1 "github.com/stakater/workshop-operator/api/v1"
 	rbac "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 // NewClusterRoleBindingSA creates a ClusterRoleBinding for Service Account
@@ -32,16 +34,38 @@ func NewClusterRoleBindingSA(workshop *workshopv1.Workshop, scheme *runtime.Sche
 	}
 
 	// Set Workshop instance as the owner and controller
-	/**
-	Error: Cross-namespace owner references are disallowed
 	err := ctrl.SetControllerReference(workshop, clusterrolebinding, scheme)
-
 	if err != nil {
 		log.Error(err, " - Failed to set SetControllerReference for ClusterRoleBinding for Service Account - %s", namespace)
 	}
-	**/
 	return clusterrolebinding
 }
+
+// GetClusterRoleBindingSA return a ClusterRoleBinding for Service Account
+func GetClusterRoleBindingSA(name string, namespace string, labels map[string]string, serviceAccountName string, roleName string, roleKind string) *rbac.ClusterRoleBinding {
+
+	clusterrolebinding := &rbac.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels:    labels,
+		},
+		Subjects: []rbac.Subject{
+			{
+				Kind:      rbac.ServiceAccountKind,
+				Name:      serviceAccountName,
+				Namespace: namespace,
+			},
+		},
+		RoleRef: rbac.RoleRef{
+			Name:     roleName,
+			Kind:     roleKind,
+			APIGroup: "rbac.authorization.k8s.io",
+		},
+	}
+	return clusterrolebinding
+}
+
 
 // NewClusterRoleBinding creates a ClusterRoleBinding for Users
 func NewClusterRoleBinding(workshop *workshopv1.Workshop, scheme *runtime.Scheme,
