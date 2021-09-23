94fe06d3004023673443763f1f9292d0508ac078
MaazMS <maazshaikh7020@gmail.com>
2021-09-23 12:25:45 +0530

    add method GetClusterRole

--- common/kubernetes/clusterrole.go
+++ common/kubernetes/clusterrole.go
@@ -1,10 +1,12 @@
 package kubernetes
 
 import (
+	"github.com/prometheus/common/log"
 	workshopv1 "github.com/stakater/workshop-operator/api/v1"
 	rbac "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 // NewClusterRole creates a ClusterRole
@@ -21,13 +23,23 @@ func NewClusterRole(workshop *workshopv1.Workshop, scheme *runtime.Scheme,
 	}
 
 	// Set Workshop instance as the owner and controller
-	/**
-	Error: cross-namespace owner references are disallowed
 	err := ctrl.SetControllerReference(workshop, clusterrole, scheme)
 	if err != nil {
 		log.Error(err, " - Failed to set SetControllerReference for ClusterRole - %s", name)
 	}
-	*/
+	return clusterrole
+}
 
+// GetClusterRole return a ClusterRole
+func GetClusterRole(name string, namespace string, labels map[string]string, rules []rbac.PolicyRule) *rbac.ClusterRole {
+
+	clusterrole := &rbac.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels:    labels,
+		},
+		Rules: rules,
+	}
 	return clusterrole
 }
