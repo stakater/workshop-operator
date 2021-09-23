5135a06cfda06974f83f859334017c5f5c270fd4
MaazMS <maazshaikh7020@gmail.com>
2021-09-23 12:29:55 +0530

    add method GetServiceAccount

--- common/kubernetes/serviceaccount.go
+++ common/kubernetes/serviceaccount.go
@@ -1,10 +1,12 @@
 package kubernetes
 
 import (
+	"github.com/prometheus/common/log"
 	workshopv1 "github.com/stakater/workshop-operator/api/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 // NewServiceAccount creates a Service Account
@@ -20,13 +22,22 @@ func NewServiceAccount(workshop *workshopv1.Workshop, scheme *runtime.Scheme,
 	}
 
 	// Set Workshop instance as the owner and controller
-	/**
-	Error: cross-namespace owner references are disallowed
 	err := ctrl.SetControllerReference(workshop, serviceaccount, scheme)
 	if err != nil {
 		log.Error(err, " - Failed to set SetControllerReference for Service Account - %s", name)
 	}
-	*/
+	return serviceaccount
+}
 
+// GetServiceAccount return Service Account
+func GetServiceAccount(name string, namespace string, labels map[string]string) *corev1.ServiceAccount {
+
+	serviceaccount := &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels:    labels,
+		},
+	}
 	return serviceaccount
 }
