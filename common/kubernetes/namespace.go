0198014fd4fd38f4c7397f681b372fbbaef6b988
MaazMS <maazshaikh7020@gmail.com>
2021-09-23 12:28:25 +0530

    add method GetNamespace

--- common/kubernetes/namespace.go
+++ common/kubernetes/namespace.go
@@ -1,10 +1,12 @@
 package kubernetes
 
 import (
+	"github.com/prometheus/common/log"
 	workshopv1 "github.com/stakater/workshop-operator/api/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 // NewNamespace creates a new namespace/project
@@ -17,12 +19,20 @@ func NewNamespace(workshop *workshopv1.Workshop, scheme *runtime.Scheme, name st
 	}
 
 	// Set Workshop instance as the owner and controller
-	/**
-	Error: cluster-scoped resource must not have a namespace-scoped owner
 	err := ctrl.SetControllerReference(workshop, namespace, scheme)
 	if err != nil {
 		log.Error(err, " - Failed to set SetControllerReference for Namespace - %s", name)
 	}
-	**/
+	return namespace
+}
+
+// GetNamespace return a namespace specified with the name
+func GetNamespace(name string) *corev1.Namespace {
+
+	namespace := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+	}
 	return namespace
 }
