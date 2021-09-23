c9b132d13c12af54b14fc425510e399fc46e3eb6
MaazMS <maazshaikh7020@gmail.com>
2021-09-23 12:24:52 +0530

    add method GetCustomResource

--- common/gitea/customresource.go
+++ common/gitea/customresource.go
@@ -1,11 +1,14 @@
 package gitea
 
 import (
+	"github.com/prometheus/common/log"
 	workshopv1 "github.com/stakater/workshop-operator/api/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
+// NewCustomResource create a new  CustomResource
 func NewCustomResource(workshop *workshopv1.Workshop, scheme *runtime.Scheme,
 	name string, namespace string, labels map[string]string) *Gitea {
 	cr := &Gitea{
@@ -22,12 +25,26 @@ func NewCustomResource(workshop *workshopv1.Workshop, scheme *runtime.Scheme,
 	}
 
 	// Set Workshop instance as the owner and controller
-	/**
-	Error: cross-namespace owner references are disallowed
 	err := ctrl.SetControllerReference(workshop, cr, scheme)
 	if err != nil {
 		log.Errorf("Failed to set SetControllerReference for %s with %s", "gitea CR", err)
 	}
-	**/
+	return cr
+}
+
+// GetCustomResource return  CustomResource
+func GetCustomResource(name string, namespace string, labels map[string]string) *Gitea {
+	cr := &Gitea{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels:    labels,
+		},
+		Spec: GiteaSpec{
+			GiteaVolumeSize:      "4Gi",
+			GiteaSsl:             true,
+			PostgresqlVolumeSize: "4Gi",
+		},
+	}
 	return cr
 }
