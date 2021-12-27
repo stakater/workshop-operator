a48efa4e0928319de4cdb9dd0b92e4292b068c2b
stakater-user <stakater@gmail.com>
2021-12-27 13:14:08 +0000

    [skip-ci] Update artifacts

--- controllers/workshop_controller.go
+++ controllers/workshop_controller.go
@@ -239,7 +239,6 @@ func (r *WorkshopReconciler) handleDelete(ctx context.Context, req ctrl.Request,
 	log := r.Log.WithValues("workshop", req.NamespacedName)
 	log.Info("Deleting workshop   " + workshop.ObjectMeta.Name)
 
-
 	if result, err := r.deleteServiceMeshService(workshop, userID); util.IsRequeued(result, err) {
 		return result, err
 	}
