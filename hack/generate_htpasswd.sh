f67d4862d3c665514a26e00f61d22eeb87e3baa3
MaazMS <maazshaikh7020@gmail.com>
2022-01-13 13:45:25 +0530

    generate htpasswd

---
+++ hack/generate_htpasswd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+OUTPUT_HTPASSWD="hack/htpasswdfile.txt"
+touch $OUTPUT_HTPASSWD
+sudo apt-get install apache2 apache2-utils -y
+p=`echo "password" | htpasswd -b -B -i -n  username`
+echo $p
+echo $p >> $OUTPUT_HTPASSWD
+cat $OUTPUT_HTPASSWD
+
