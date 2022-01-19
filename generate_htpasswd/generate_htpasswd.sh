9709e98417880b11e088305120aa9aab568387a6
MaazMS <maazshaikh7020@gmail.com>
2022-01-19 17:20:15 +0530

    update htpasswd shell script

--- generate_htpasswd/generate_htpasswd.sh
+++ generate_htpasswd/generate_htpasswd.sh
@@ -2,7 +2,7 @@
 
 OUTPUT_HTPASSWD="generate_htpasswd/htpasswdfile.txt"
 touch $OUTPUT_HTPASSWD
-sudo apt-get install apache2 apache2-utils -y
+apt-get install apache2 apache2-utils -y
 p=`echo "password" | htpasswd -b -B -i -n  username`
 echo $p
 echo $p >> $OUTPUT_HTPASSWD
