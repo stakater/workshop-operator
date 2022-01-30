6611c7d0f11be5e6d6a2d9715a84b468879626d1
Maaz <34999445+MaazMS@users.noreply.github.com>
2022-01-30 20:59:52 +0530

    Delete operation of  serverless (#41)
    * add delete method of serverless
    
    * add serverless
    
    * create const for serverless namespace,subscription ect
    
    * add delete method of serverless
    
    * add delete method of serverless
    
    * add delete method of serverless
    
    * remove unnecessary log

--- controllers/workshop_controller.go
+++ controllers/workshop_controller.go
@@ -239,6 +239,10 @@ func (r *WorkshopReconciler) handleDelete(ctx context.Context, req ctrl.Request,
 	log := r.Log.WithValues("workshop", req.NamespacedName)
 	log.Info("Deleting workshop   " + workshop.ObjectMeta.Name)
 
+	if result, err := r.deleteServerless(workshop); util.IsRequeued(result, err) {
+		return result, err
+	}
+
 	if result, err := r.deleteServiceMeshService(workshop, userID); util.IsRequeued(result, err) {
 		return result, err
 	}
